cbd600f3b6d40fc3c2076d99d84b10cb407a69b9
xuech <xuech_love@126.com>
2019-05-23 16:08:43 +0800

    tabitem及文件夹搭建，资源加载

---
+++ src/pages/me/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    data () {
+      return {
+        motto: '',
+      }
+    },
+
+    components: {
+    },
+
+    methods: {
+    },
+
+    created () {
+
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
